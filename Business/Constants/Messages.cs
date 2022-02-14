e91cd83c284c2e3151347e7b3fa477922313a048
İLKER BAYKARA <ilkerbaykara2@gmail.com>
2022-02-14 17:24:39 +0300

    10.Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static readonly string ProductAdded = "Ürün eklendi";
+        public static readonly string ProductNameInvalid= "Geçersiz isim";
+        public static readonly string ProductsGet = "Ürünler listelendi";
+        public static readonly string MaintenanceTime = "Sistemde bakım var";
+    }
+}
